4aa974cb66cb30b0070ea0e054f929c0df0b0dcf
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-07-21 20:02:12 +0200

    Update core.js

--- static/js/core.js
+++ static/js/core.js
@@ -1,6 +1,28 @@
-function reloadImageTwice(imageId) {
-    const img = document.getElementById(imageId);
-    const src = img.src;
-    img.src = ''; // First set to empty
-    img.src = src; // Then set to original src
+function reloadImage(img) {
+    let originalSrc = img.src;
+    img.src = '';
+    img.src = originalSrc;
+}
+
+function reloadImages(times) {
+    let images = document.querySelectorAll('img');
+    for (let i = 0; i < times; i++) {
+        images.forEach(function(img) {
+            reloadImage(img);
+        });
+    }
+}
+
+function getLocationAndRedirect() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            window.location.href = `/set_location?cityname=Votre position&longitude=${longitude}&latitude=${latitude}`;
+        }, function(error) {
+            alert('Erreur lors de la récupération de la localisation: ' + error.message);
+        });
+    } else {
+        alert('La géolocalisation n\'est pas supportée par ce navigateur.');
+    }
 }
