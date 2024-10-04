68e7ecb0f0f15757ee74a23497cfb2d905831dd9
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-10-04 16:28:01 +0200

    Modification des paramètres

--- docker-init.py
+++ docker-init.py
@@ -5,22 +5,20 @@ from colorama import init, Fore
 init(autoreset=True)
 
 workdir = "/data"
+default_settings = {
+    "api": "api.open-meteo.com",
+    # Proxy (https://docs.proxyscrape.com/)
+    "use_proxies": True,
+    "proxy_country_code": "all",
+    "proxy_max_timeout": 50,
+    # Personnalisation
+    "footer": ""
+}
 
 if not os.path.exists(f"{workdir}/config.yml"):
 
     print(Fore.YELLOW + "Création du fichier de paramètres...")
 
     with open(f"{workdir}/config.yml", "w", encoding="utf-8") as f:
-        yaml.safe_dump(
-            {
-                "api": "api.open-meteo.com",
-                "server_description": "",
-                # Pour remplir les paramètres ci-dessous, veuillez consulter https://docs.proxyscrape.com/
-                "use_proxies": True,
-                "proxy_country_code": "all",
-                "proxy_max_timeout": 50
-            },
-            f,
-            default_flow_style=False
-        )
+        yaml.safe_dump(default_settings, f, default_flow_style=False)
         f.close()
